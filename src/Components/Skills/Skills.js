a1f52471c16d009e8c8c4ac9d3c1153e43e15642
Sami Rahman <46711771+sami246@users.noreply.github.com>
2023-11-09 14:09:52 +0000

    Finished Skills Section

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import reactImage from '../../assets/logos/react.svg';
+import photoshopImage from '../../assets/logos/adobe-photoshop-cc.svg';
+import './Skills.css'
+import { main_skills, secondary_skills } from '../../data/SkillsData';
 
 export const Skills = () => {
     return (
         <div id='boxContainer'>
             <header className='boxHeading'>Skills</header>
+            <h4>Primary Skills</h4>
+            <div className='skillsContainer'>
+                {main_skills?.map((item, index) => (
+                    <div className='skillsBox'>
+                        <div className='img_description'>{item.description} </div>
+                        <img key={index} src={item.logo} alt={item.description} width='100%' height='100%' />
+                    </div>
+                ))}
+            </div>
+            <h4>Secondary Skills</h4>
+            <div className='skillsContainer'>
+                {secondary_skills?.map((item, index) => (
+                    <div className='skillsBoxSmall'>
+                        <div className='img_description'>{item.description} </div>
+                        <img key={index} src={item.logo} alt={item.description} width='100%' height='100%' />
+                    </div>
+
+                ))}
+            </div>
         </div>
-    )
+    );
 }
---
+++ src/data/SkillsData.js
@@ -0,0 +1,109 @@
+import reactImage from '../assets/logos/react.svg';
+import photoshopImage from '../assets/logos/adobe-photoshop-cc.svg';
+import css3Image from '../assets/logos/css3.svg';
+import firebaseImage from '../assets/logos/firebase.svg';
+import supabaseImage from '../assets/logos/supabase.svg';
+import htmlImage from '../assets/logos/html5.svg';
+import javascriptImage from '../assets/logos/javascript-js.svg';
+import expoImage from '../assets/logos/expo.svg';
+import javaImage from '../assets/logos/java.png';
+// import jsonImage from '../assets/logos/json.svg';
+import nodejsImage from '../assets/logos/nodejs.svg';
+// import npmImage from '../assets/logos/npm-node-package-manager.svg';
+import phpImage from '../assets/logos/php.svg';
+import reactNativeImage from '../assets/logos/react-native.svg';
+import pythonImage from '../assets/logos/python.svg';
+import wixImage from '../assets/logos/wix.svg'
+import iosImage from '../assets/logos/apple-ios.svg'
+import googlePlayImage from '../assets/logos/google-playstore.svg'
+import youtubeImage from '../assets/logos/youtube.svg'
+import mysqlImage from '../assets/logos/mysql.svg'
+import djangoImage from '../assets/logos/django.svg'
+
+export const main_skills = [
+    {
+        logo: javascriptImage,
+        description: 'Javascript JS'
+    },
+
+    {
+        logo: htmlImage,
+        description: 'HTML 5'
+    },
+    {
+        logo: reactImage,
+        description: 'React JS'
+    },
+    {
+        logo: firebaseImage,
+        description: 'Firebase'
+    },
+    {
+        logo: supabaseImage,
+        description: 'Supabase'
+    },
+
+    {
+        logo: pythonImage,
+        description: 'Python'
+    },
+    {
+        logo: css3Image,
+        description: 'CSS 3'
+
+    },
+    {
+        logo: expoImage,
+        description: 'Expo'
+    },
+    {
+        logo: reactNativeImage,
+        description: 'React Native'
+    },
+
+]
+
+export const secondary_skills = [
+    {
+        logo: photoshopImage,
+        description: 'Photoshop CC'
+    },
+    {
+        logo: iosImage,
+        description: 'Apple Connect Store'
+    },
+    {
+        logo: googlePlayImage,
+        description: 'Google Play Store'
+    },
+    {
+        logo: javaImage,
+        description: 'Java'
+    },
+    {
+        logo: youtubeImage,
+        description: 'Youtube'
+    },
+    {
+        logo: nodejsImage,
+        description: 'Node JS'
+    },
+    {
+        logo: wixImage,
+        description: 'Wix'
+    },
+    {
+        logo: phpImage,
+        description: 'PHP'
+    },
+    {
+        logo: djangoImage,
+        description: 'Django'
+    },
+    {
+        logo: mysqlImage,
+        description: 'mySQL'
+    },
+
+
+]
