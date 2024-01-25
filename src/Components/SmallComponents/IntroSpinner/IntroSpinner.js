4132257cf444c5bee572aff380ff132fafd8f6fa
Sami Rahman <46711771+sami246@users.noreply.github.com>
2024-01-25 12:56:41 +0000

    Fix Intro Spinner Responsiveness

--- src/Components/SmallComponents/IntroSpinner/IntroSpinner.js
+++ src/Components/SmallComponents/IntroSpinner/IntroSpinner.js
@@ -5,11 +5,13 @@ export const IntroSpinner = () => {
     return (
         <main class="intro-spinner-container">
             <p>Hello 👋 I'm</p>
-            <section class="animation">
-                <div class="first"><div>Sami Rahman</div></div>
-                <div class="second"><div>A Software Developer</div></div>
-                <div class="third"><div>Looking for Work</div></div>
-            </section>
+            <div className='spinner-container'>
+                <section class="animation">
+                    <div class="first"><div className='box'>Sami Rahman</div></div>
+                    <div class="second"><div className='box'>A Software Developer</div></div>
+                    <div class="third"><div className='box'>Looking for Work</div></div>
+                </section>
+            </div>
         </main>
     )
 }
