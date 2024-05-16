4624754dd984810ef82e5b55105d36b9c8ca304a
Sami Rahman <46711771+sami246@users.noreply.github.com>
2024-05-16 11:01:58 +0100

    Added Vercel Analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import { Analytics } from '@vercel/analytics/react';
 import './App.css';
 import { AppProvider } from './contexts/AppContext';
 import { MainApp } from './MainApp';
 
+
+
 function App() {
   return (
     <AppProvider>
+      <Analytics />
       <MainApp />
     </AppProvider>
   );
