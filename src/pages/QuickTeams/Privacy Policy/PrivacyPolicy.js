5475f824432a0e5cec8ecc7c9012bb0d1246cbd7
Sami Rahman <46711771+sami246@users.noreply.github.com>
2025-01-06 18:50:34 +0000

    Added Quick Teams Privacy Policy

---
+++ src/pages/QuickTeams/Privacy Policy/PrivacyPolicy.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import './PrivacyPolicy.css';
+
+const PrivacyPolicy = () => {
+    return (
+        <div className='PrivacyContainer'>
+            <div className='headingText'>Privacy Policy</div>
+
+            <div className='boldText'>Last updated: [06/01/2025]</div>
+            <div className='heading2' style={{ textAlign: 'center' }}>Quick Teams</div>
+
+            This Privacy Policy describes Our policies and procedures on the collection, use, and disclosure of Your information when You use the Service and explains Your privacy rights and how the law protects You.
+            <br />
+            We use Your Personal Data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy.
+
+            <div className='heading2'>Interpretation and Definitions</div>
+            <div className='heading3' style={{ paddingTop: 0 }}>Interpretation</div>
+            The words with initial capital letters have specific meanings defined under the following conditions. These definitions apply whether the words appear in singular or plural.
+
+            <div className='heading3'>Definitions</div>
+            For the purposes of this Privacy Policy:
+            <br />
+            <span className='bulletPoint boldText'>- Account:</span> A unique account created for You to access Our Service.
+            <br />
+            <span className='bulletPoint boldText'>- Application:</span> Refers to Quick Teams, the software program provided by the Company.
+            <br />
+            <span className='bulletPoint boldText'>- Company:</span> (referred to as "We", "Us", or "Our") refers to Quick Teams.
+            <br />
+            <span className='bulletPoint boldText'>- Device:</span> Any device that can access the Service, such as a computer, phone, or tablet.
+            <br />
+            <span className='bulletPoint boldText'>- Personal Data:</span> Any information relating to an identified or identifiable individual.
+            <br />
+            <span className='bulletPoint boldText'>- Service:</span> Refers to the Quick Teams Application.
+            <br />
+            <span className='bulletPoint boldText'>- You:</span> The individual accessing or using the Service, or the company/legal entity on whose behalf the Service is being used.
+
+            <div className='heading2'>Collecting and Using Your Personal Data</div>
+            <div className='heading3' style={{ paddingTop: 0 }}>Types of Data Collected:</div>
+
+            <div className='heading4'>Personal Data</div>
+            While using Our Service, We may ask You to provide certain information, including but not limited to:
+            <br />
+            <span className='bulletPoint boldText'>- Email address</span>
+            <br />
+            <span className='bulletPoint boldText'>- First and last name</span>
+            <br />
+            <span className='bulletPoint boldText'>- Usage Data</span>
+            <br />
+
+            <div className='heading4'>Usage Data</div>
+            Usage Data is collected automatically when using the Service and may include:
+            <br />
+            <span className='bulletPoint'>- Your Device's IP address</span>
+            <br />
+            <span className='bulletPoint'>- Browser type and version</span>
+            <br />
+            <span className='bulletPoint'>- Pages You visit, time spent on pages, and diagnostic data</span>
+            <br />
+
+            <div className='heading3'>Use of Your Personal Data</div>
+            The Company may use Your Personal Data to:
+            <br />
+            <span className='bulletPoint boldText'>- Provide and maintain the Service</span>
+            <br />
+            <span className='bulletPoint boldText'>- Notify You of updates and changes</span>
+            <br />
+            <span className='bulletPoint boldText'>- Analyze usage trends to improve functionality</span>
+            <br />
+
+            <div className='heading3'>Retention of Your Data</div>
+            We retain Your data only as long as necessary for purposes outlined in this Privacy Policy or as required by law.
+
+            <div className='heading3'>Children's Privacy</div>
+            Quick Teams does not knowingly collect personal data from individuals under the age of 13. If You become aware of such data being provided, please contact Us.
+
+            <div className='heading3'>Contact Us</div>
+            If You have questions about this Privacy Policy, You can contact Us at: [thefoodiiofficial@gmail.com].
+        </div>
+    );
+};
+
+export default PrivacyPolicy;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom';
 import { MainApp } from '../MainApp';
-import ProjectPage from '../pages/ProjectPage/ProjectPage';
 import NotFound from '../pages/Error404/NotFound';
+import PrivacyPolicy from '../pages/QuickTeams/Privacy Policy/PrivacyPolicy';
 
 const index = () => {
     return (
         <Routes>
             {/*  Dashboard */}
             <Route path='/' element={<MainApp />} />
+            <Route path='/quick-teams/privacy-policy' element={<PrivacyPolicy />} />
             {/* <Route path='/individual-project/:projectID' element={<ProjectPage />} /> */}
             <Route path="*" element={<NotFound />} />
         </Routes>
