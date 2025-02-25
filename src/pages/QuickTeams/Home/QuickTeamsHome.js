999a63db06806b920c440c3dd8e49f7722e3dd8d
Sami Rahman <46711771+sami246@users.noreply.github.com>
2025-02-25 14:17:17 +0000

    Added Buttons to navigate to stores

--- src/pages/QuickTeams/Home/QuickTeamsHome.js
+++ src/pages/QuickTeams/Home/QuickTeamsHome.js
@@ -1,56 +1,98 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../../assets/logos/quick-teams-logo.png';
+import banner from '../../../assets/project_images/Quick Teams 1.jpg';
+import { FaGooglePlay, FaAppStore } from 'react-icons/fa';
 
 const QuickTeamsHome = () => {
   const styles = {
     container: {
-      fontFamily: 'Arial, sans-serif',
-      padding: '20px',
+      // fontFamily: 'Arial, sans-serif',
       backgroundColor: '#f5fcfd',
       color: '#013774',
-      height: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+      minHeight: '100vh',
+      padding: '20px',
     },
     contentContainer: {
       maxWidth: '800px',
-      margin: '0 auto',
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
     },
     header: {
       textAlign: 'center',
-      marginBottom: '30px',
+      marginBottom: '20px',
     },
     title: {
-      fontSize: '28px',
+      fontSize: '24px',
       fontWeight: 'bold',
       color: '#1896ec',
+      marginBottom: '10px',
     },
     description: {
-      fontSize: '18px',
-      lineHeight: '1.6',
-      marginBottom: '20px',
+      fontSize: '16px',
+      lineHeight: '1.5',
+      marginBottom: '15px',
+      textAlign: 'center',
     },
     privacyLink: {
       color: '#0c72cf',
       textDecoration: 'underline',
     },
     logo: {
-      maxWidth: '200px',
+      maxWidth: '150px',
       display: 'block',
-      margin: '20px auto',
+      margin: '10px auto',
+      borderRadius: '8px',
     },
     dataInfo: {
-      fontSize: '16px',
-      lineHeight: '1.6',
+      fontSize: '14px',
+      lineHeight: '1.4',
+      marginBottom: '15px',
+      textAlign: 'center',
+    },
+    banner: {
+      width: '100%',
+      // maxHeight: '300px',
+      objectFit: 'contain',
       marginBottom: '20px',
+      borderRadius: '4px',
+    },
+    downloadButtons: {
+      display: 'flex',
+      justifyContent: 'center',
+      marginBottom: '20px',
+    },
+    downloadButton: {
+      margin: '0 10px',
+      padding: '10px 20px',
+      backgroundColor: '#e0f2f7',
+      borderRadius: '5px',
+      display: 'flex',
+      alignItems: 'center',
+      textDecoration: 'none',
+      color: '#013774',
+      border: '1px solid #caf0f7',
+      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+    },
+    downloadIcon: {
+      fontSize: '1.5em',
+      marginRight: '8px',
     },
   };
 
   return (
     <div style={styles.container}>
       <div style={styles.contentContainer}>
+        <img src={banner} alt="Quick Teams Banner" style={styles.banner} />
         <div style={styles.header}>
           <img src={logo} alt="Quick Teams Logo" style={styles.logo} />
-          <h1 style={styles.title}>Welcome to Quick Teams</h1>
+          <h1 style={styles.title}>Quick Teams</h1>
         </div>
         <p style={styles.description}>
           Quick Teams is a random team generator application that allows you to rank players on a level system. Based on their ratings, evenly rated teams will be generated from the players you select.
@@ -63,6 +105,17 @@ const QuickTeamsHome = () => {
         <p>
           Read our <Link to="/quick-teams/privacy-policy" style={styles.privacyLink}>Privacy Policy</Link>.
         </p>
+
+        <div style={styles.downloadButtons}>
+          <a target='_blank' href="https://play.google.com/store/apps/details?id=com.SRMakes.QuickTeams" style={styles.downloadButton}>
+            <FaGooglePlay style={styles.downloadIcon} />
+            <span>Google Play</span>
+          </a>
+          <a target='_blank' href="https://apps.apple.com/us/app/quick-teams-team-generator/id6740728374" style={styles.downloadButton}>
+            <FaAppStore style={styles.downloadIcon} />
+            <span>Apple Store</span>
+          </a>
+        </div>
       </div>
     </div>
   );
